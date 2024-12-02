64146efa992b1fbf0f1fea194f6b61a72722a5a4
Renan Bernordi <altendorfme@gmail.com>
2024-12-02 17:01:20 -0300

    hotfix cron container name

--- cronjob.sh
+++ cronjob.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "[$(date)] Running cron job"
-curl -s -H "Authorization: Bearer $CRON_SECRET" "http://localhost:3000/api/check"
+curl -s -H "Authorization: Bearer $CRON_SECRET" "http://marreta-bot:3000/api/check"
 echo "[$(date)] Cron job completed"
